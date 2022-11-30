da3c97b83a751e8fae4c943b8fd48775903f0a2d
nizniashard <nizniashard1505@gmail.com>
2022-11-30 16:37:05 +0530

    added responsiveness via media query

--- main.js
+++ main.js
@@ -59,20 +59,6 @@ setInterval(() => {
     flipCard(secondsDiv, secondsRemaining);
     globalSecondsRemaining = secondsRemaining;
   }
-
-  if (false) {
-    console.log("\n");
-    console.log("days remaining-->" + daysRemaining);
-    console.log("hours remaining-->" + hoursRemaining);
-    console.log("minutes remaining-->" + minutesRemaining);
-    console.log("seconds remaining-->" + secondsRemaining);
-  }
-
-  //   console.log(timeRemaining.padStart(2, "0"));
-  //   console.log(currentTime.toString());
-  // console.log(countDownTo);
-  //   const timeRemainingDate = new Date(timeRemaining);
-  //   console.log(timeRemainingDate.toString());
 }, 250);
 
 function getDaysRemaining(remainingTimeMs) {
@@ -118,52 +104,3 @@ function flipCard(tileContainer, currentNumber) {
     bottomDiv.innerHTML = nextNumber;
   });
 }
-
-// flipCard()
-
-// setInterval(() => {
-const tileContainer = document.querySelector(".tile-container");
-// flipCard(tileContainer, 56);
-// }, 3000);
-
-// bottomDiv.innerHTML = "55";
-
-// tileTop.classList.add("flip-top");
-// tileBottom.classList.add("flip-bottom");
-// for (var i = 0; i < tile.children.length; i++) {
-//   var child = tile.children[i];
-//   // console.log(child);
-//   // console.log(child.textContent);
-//   child.textContent = "55";
-// }
-
-// setInterval(() => {
-//   var countdown_end = new Date((year = 2022), (month = 11), (date = 1));
-//   var current_time = new Date(Date.now());
-//   var time_remaining = new Date(countdown_end - current_time);
-
-//   // update the inner html
-//   const days_remaining = time_remaining.getDate();
-//   const hours_remaining = time_remaining.getHours();
-//   const minutes_remaining = time_remaining.getMinutes();
-//   const seconds_remaining = time_remaining.getSeconds();
-
-//   const days_element = document.getElementById("days-counter");
-//   days_element.innerHTML = days_remaining;
-
-//   const hours_element = document.getElementById("hours-counter");
-//   hours_element.innerHTML = hours_remaining;
-
-//   const minutes_element = document.getElementById("minutes-counter");
-//   minutes_element.innerHTML = minutes_remaining;
-
-//   const seconds_element = document.getElementById("seconds-counter");
-//   seconds_element.innerHTML = seconds_remaining;
-
-//   console.log("countdown end-->" + countdown_end);
-//   console.log("current time-->" + current_time);
-//   console.log("days remaining-->" + time_remaining.getDate());
-//   console.log("hours remaining-->" + time_remaining.getHours());
-//   console.log("minutes remaining-->" + time_remaining.getMinutes());
-//   console.log("seconds remaining-->" + time_remaining.getSeconds());
-// }, 1000);
